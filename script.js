53c1ab7593fb45fc2c96401ff8acdb0e3da18093
nureddyn <nureddynballout@gmail.com>
2023-11-02 18:15:22 -0300

    Lab done

--- script.js
+++ script.js
@@ -17,32 +17,50 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+  const mainTitle = document.querySelector('#main-title');
+  mainTitle.textContent = "I'm DOM Toretto, and this is my Page";
 
   // Part 2
-
+  const body = document.querySelector('body');
+  body.style.backgroundColor = 'Grey';
 
   // Part 3
-
+  const favoriteThings = document.querySelector('#favorite-things');
+  favoriteThings.removeChild(favoriteThings.children[5]);
 
   // Part 4
-
+  document.querySelectorAll('.special-title').forEach((title) => {
+    title.style.fontSize = '2rem';
+  });
 
   // Part 5
-
+  const pastRaces = document.querySelector('#past-races');
+  pastRaces.removeChild(pastRaces.children[3]);
 
   // Part 6
-
+  const newLi = document.createElement('li');
+  newLi.innerHTML = 'Pittsburgh';
+  pastRaces.append(newLi);
 
   // Part 7
-
+  const newPost = document.createElement('div');
+  newPost.classList.add('blog-post');
+  newPost.classList.toggle('purple');
+  let newH1 = document.createElement('h1');
+  newH1.innerHTML = newLi.innerHTML;
+  let paragraph = document.createElement('p');
+  paragraph.textContent = 'I JUMPED WITH MY CAR FROM MOUNT WASHINGTON TO THE OHIO RIVER!';
+  newPost.append(newH1, paragraph);
+  document.querySelector('.main').append(newPost);
 
   // Part 8
-
+  document.querySelector('#quote-title').onclick = () => {
+    randomQuote();
+  };
 
   // Part 9
-
-
-
-
+  document.querySelectorAll('.blog-post').forEach((post) => {
+    post.addEventListener('mouseout', () => {post.classList.toggle('purple')});
+    post.addEventListener('mouseenter', () => {post.classList.toggle('red')});
+  });
 });
